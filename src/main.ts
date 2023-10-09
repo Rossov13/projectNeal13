a1da2a33e45b8d402d91b3d6964e4d9c1714ccca
Neal Rossov <nealrossov@gmail.com>
2023-10-09 15:23:51 +0300

    Modul_15_16_week21

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
 import './style.css'
 
+// JS_PZ_Module_15_16_Week_21_1548323208.pdf
 
+// 1.
+const tabs = document.querySelectorAll('[data-tab-value]')
+const tabInfos = document.querySelectorAll('[data-tab-info]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabValue);  
+        tabInfos.forEach(tabInfo => {
+            tabInfo.classList.remove('active')
+        })
+        target.classList.add('active');
+    })
+})
+
+// 2.
