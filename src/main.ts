89314563c16e14c2d60803745a2c0fbc7555601a
neal rossov <nealrossov@gmail.com>
2023-10-13 19:17:05 +0300

    13.10.2023

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,18 @@ import './style.css'
 // JS_PZ_Module_15_16_Week_21_1548323208.pdf
 
 // 1.
-const tabs = document.querySelectorAll('[data-tab-value]')
+const tabs = document.querySelectorAll('[data-tab-value]') as NodeListOf<HTMLElement>
 const tabInfos = document.querySelectorAll('[data-tab-info]')
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.tabValue);  
-        tabInfos.forEach(tabInfo => {
-            tabInfo.classList.remove('active')
-        })
-        target.classList.add('active');
+        if (tab.dataset.tabValue) {
+            const target = document.getElementById(tab.dataset.tabValue)
+            tabInfos.forEach(tabInfo => {
+                tabInfo.classList.remove('active')
+            })
+            if (target) target.classList.add('active')
+        }
     })
 })
 
