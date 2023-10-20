d08b8975b90ab21a35da5419906ccb89fcfeccb4
neal rossov <nealrossov@gmail.com>
2023-10-20 21:30:02 +0300

    20.10.2023

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,15 @@ const tabs = document.querySelectorAll('[data-tab-value]') as NodeListOf<HTMLEle
 const tabInfos = document.querySelectorAll('[data-tab-info]')
 
 tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        if (tab.dataset.tabValue) {
-            const target = document.getElementById(tab.dataset.tabValue)
-            tabInfos.forEach(tabInfo => {
-                tabInfo.classList.remove('active')
-            })
-            if (target) target.classList.add('active')
-        }
-    })
+  tab.addEventListener('click', () => {
+    if (tab.dataset.tabValue) {
+      const target = document.getElementById(tab.dataset.tabValue)
+      tabInfos.forEach(tabInfo => {
+        tabInfo.classList.remove('active')
+      })
+      if (target) target.classList.add('active')
+    }
+  })
 })
 
 
@@ -28,12 +28,12 @@ const dialog = document.getElementById("favDialog") as HTMLDialogElement;
 // const confirm = dialog.querySelector("[type=submit]") as HTMLElement;
 const select = dialog.querySelector("#favAnimal") as HTMLSelectElement
 
-dialog.addEventListener('close', (e)=>{
-    if (dialog.returnValue=='cancel') {
-        console.log("Canceled");
-    } else {
-        console.log(select.value)
-    }
+dialog.addEventListener('close', (e) => {
+  if (dialog.returnValue == 'cancel') {
+    console.log("Canceled");
+  } else {
+    console.log(select.value)
+  }
 })
 
 
@@ -46,7 +46,7 @@ dialog.addEventListener('close', (e)=>{
 // }
 
 updateButton.addEventListener("click", () => {
-    dialog.showModal();
+  dialog.showModal();
 });
 
 
@@ -67,3 +67,52 @@ updateButton.addEventListener("click", () => {
 // 3. Text field with autocomplete.
 // 4. User rating score card
 
+const rating = {
+  "header": "User rating",
+  "maxvalue": 254,
+  "data": [
+    {
+      "title": "5 stars",
+      "value": 150
+    },
+    {
+      "title": "4 stars",
+      "value": 63
+    },
+    {
+      "title": "3 stars",
+      "value": 15
+    },
+    {
+      "title": "2 stars",
+      "value": 6
+    },
+    {
+      "title": "1 star",
+      "value": 20
+    }
+  ]
+}
+
+const ratingDiv = document.querySelector('.userRating') as HTMLDivElement
+
+function renderRating() {
+  ratingDiv.innerHTML = ''
+  ratingDiv.innerHTML += `<span class="heading">${rating.header}</span>`
+  ratingDiv.innerHTML += `<hr style="border: 3px solid #f1f1f1">`
+  rating.data.forEach(el=>{
+    ratingDiv.innerHTML += `<div class="side">
+    <div>${el.title}</div>
+  </div>
+  <div class="middle">
+    <div class="bar-container">
+      <div style="width:${el.value/rating.maxvalue*100}%" class="bar-${el.title[0]}"></div>
+    </div>
+  </div>
+    <div class="side right">
+      <div>${el.value}</div>
+    </div>`
+  })
+}
+
+renderRating()
