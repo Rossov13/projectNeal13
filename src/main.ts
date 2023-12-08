0e27fcb2fd3159cf04255d4dcac6d34b01832c95
neal rossov <nealrossov@gmail.com>
2023-12-08 20:54:31 +0300

    1

--- src/main.ts
+++ src/main.ts
@@ -197,6 +197,36 @@ stars.forEach((star, index1) => {
 });
 
 
+// let myNodelist = document.getElementsByTagName("LI");
+// let i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   let span = document.createElement("SPAN");
+//   let txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
+
+
+// let closebtns = document.getElementsByClassName("close");
+// let k;
+
+// for (k = 0; k < closebtns.length; k++) {
+//   closebtns[k].addEventListener("click", function() {
+//     this.parentElement.style.display = 'none';
+//   });
+// }
+
+
+// const todoUL = document.getElementById('myUL')
+// const addBtn = document.querySelector('.addBtn')
+// addBtn?.addEventListener('click', addNewElement)
+
+
+// function addNewElement() {
+//   todoUL?.insertAdjacentHTML('afterbegin', `<li>${(document.getElementById("myInput")as HTMLInputElement)?.value}<span class="close">\u00D7</span></li>`)
+// }
+
 let myNodelist = document.getElementsByTagName("LI");
 let i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -207,20 +237,33 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-let closebtns = document.getElementsByClassName("close");
-let k;
-
-for (k = 0; k < closebtns.length; k++) {
-  closebtns[k].addEventListener("click", function() {
-    this.parentElement.style.display = 'none';
-  });
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+let i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
 }
 
+// Add a "checked" symbol when clicking on a list item
 
-function newElement() {
-  let li = document.createElement("li");
-  let inputValue = document.getElementById("myInput").value;
-  let t = document.createTextNode(inputValue);
-  
+let list = document.querySelector('ul') as HTMLElement
+  list.addEventListener('click', function(ev) {
+
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+const todoUL = document.getElementById('myUL')
+const addBtn = document.querySelector('.addBtn')
+addBtn?.addEventListener('click', addNewElement)
+
+
+function addNewElement() {
+  todoUL?.insertAdjacentHTML('afterbegin', `<li>${(document.getElementById("myInput")as HTMLInputElement)?.value}<span class="close">\u00D7</span></li>`)
 }
 
