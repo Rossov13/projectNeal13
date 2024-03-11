f3e6d6eb598fd1145c184eb47a0ad6ffb4e7f2f0
neal rossov <nealrossov@gmail.com>
2024-03-11 21:29:10 +0300

    11.03.2024

--- src/main.ts
+++ src/main.ts
@@ -22,32 +22,32 @@ tabs.forEach(tab => {
 
 
 // 2. Modal window.
-const updateButton = document.getElementById('updateDetails') as HTMLElement;
-const dialog = document.getElementById("favDialog") as HTMLDialogElement;
-// const cancelButton = dialog.querySelector("[type=reset]") as HTMLElement;
-// const confirm = dialog.querySelector("[type=submit]") as HTMLElement;
-const select = dialog.querySelector("#favAnimal") as HTMLSelectElement
+// const updateButton = document.getElementById('updateDetails') as HTMLElement;
+// const dialog = document.getElementById("favDialog") as HTMLDialogElement;
+// // const cancelButton = dialog.querySelector("[type=reset]") as HTMLElement;
+// // const confirm = dialog.querySelector("[type=submit]") as HTMLElement;
+// const select = dialog.querySelector("#favAnimal") as HTMLSelectElement
 
-dialog.addEventListener('close', (e) => {
-  if (dialog.returnValue == 'cancel') {
-    console.log("Canceled");
-  } else {
-    console.log(select.value)
-  }
-})
+// dialog.addEventListener('close', (e) => {
+//   if (dialog.returnValue == 'cancel') {
+//     console.log("Canceled");
+//   } else {
+//     console.log(select.value)
+//   }
+// })
 
 
-// function openCheck(dialog: any) {
-//     if (dialog.open) {
-//         console.log("Dialog open");
-//     } else {
-//         console.log("Dialog closed");
-//     }
-// }
+// // function openCheck(dialog: any) {
+// //     if (dialog.open) {
+// //         console.log("Dialog open");
+// //     } else {
+// //         console.log("Dialog closed");
+// //     }
+// // }
 
-updateButton.addEventListener("click", () => {
-  dialog.showModal();
-});
+// updateButton.addEventListener("click", () => {
+//   dialog.showModal();
+// });
 
 
 // cancelButton.addEventListener("click", () => {
@@ -67,55 +67,55 @@ updateButton.addEventListener("click", () => {
 // 3. Text field with autocomplete.
 // 4. User rating score card
 
-const rating = {
-  "header": "User rating",
-  "maxvalue": 254,
-  "data": [
-    {
-      "title": "5 stars",
-      "value": 150
-    },
-    {
-      "title": "4 stars",
-      "value": 63
-    },
-    {
-      "title": "3 stars",
-      "value": 15
-    },
-    {
-      "title": "2 stars",
-      "value": 6
-    },
-    {
-      "title": "1 star",
-      "value": 20
-    }
-  ]
-}
+// const rating = {
+//   "header": "User rating",
+//   "maxvalue": 254,
+//   "data": [
+//     {
+//       "title": "5 stars",
+//       "value": 150
+//     },
+//     {
+//       "title": "4 stars",
+//       "value": 63
+//     },
+//     {
+//       "title": "3 stars",
+//       "value": 15
+//     },
+//     {
+//       "title": "2 stars",
+//       "value": 6
+//     },
+//     {
+//       "title": "1 star",
+//       "value": 20
+//     }
+//   ]
+// }
 
-const ratingDiv = document.querySelector('.userRating') as HTMLDivElement
+// const ratingDiv = document.querySelector('.userRating') as HTMLDivElement
 
-function renderRating() {
-  ratingDiv.innerHTML = ''
-  ratingDiv.innerHTML += `<span class="heading">${rating.header}</span>`
-  ratingDiv.innerHTML += `<hr style="border: 3px solid #f1f1f1">`
-  rating.data.forEach(el => {
-    ratingDiv.innerHTML += `<div class="side">
-    <div>${el.title}</div>
-  </div>
-  <div class="middle">
-    <div class="bar-container">
-      <div style="width:${el.value / rating.maxvalue * 100}%" class="bar-${el.title[0]}"></div>
-    </div>
-  </div>
-    <div class="side right">
-      <div>${el.value}</div>
-    </div>`
-  })
-}
+// function renderRating() {
+//   ratingDiv.innerHTML = ''
+//   ratingDiv.innerHTML += `<span class="heading">${rating.header}</span>`
+//   ratingDiv.innerHTML += `<hr style="border: 3px solid #f1f1f1">`
+//   rating.data.forEach(el => {
+//     ratingDiv.innerHTML += `<div class="side">
+//     <div>${el.title}</div>
+//   </div>
+//   <div class="middle">
+//     <div class="bar-container">
+//       <div style="width:${el.value / rating.maxvalue * 100}%" class="bar-${el.title[0]}"></div>
+//     </div>
+//   </div>
+//     <div class="side right">
+//       <div>${el.value}</div>
+//     </div>`
+//   })
+// }
 
-renderRating()
+// renderRating()
 
 const cards = document.querySelectorAll('.memory-card') as NodeListOf<HTMLDivElement>
 const startGame = document.querySelector('#startGame') as HTMLButtonElement
