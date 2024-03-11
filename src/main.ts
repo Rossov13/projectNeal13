75548d51315acb35e73d9f4d26521a8b22e3f31f
neal rossov <nealrossov@gmail.com>
2024-03-11 20:24:17 +0300

    breaking

--- src/main.ts
+++ src/main.ts
@@ -377,35 +377,63 @@ cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates
 
 //Game Brick Destroyer
 
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-const dx = 2;
-const dy = -2;
+// Include api for currency change
+const api = "https://api.exchangerate-api.com/v4/latest/USD";
 
-function drawBall() {
-  ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-}
+// For selecting different controls
+let search = document.querySelector(".searchBox");
+let convert = document.querySelector(".convert");
+let fromCurrecy = document.querySelector(".from");
+let toCurrecy = document.querySelector(".to");
+let finalValue = document.querySelector(".finalValue");
+let finalAmount = document.getElementById("finalAmount");
+let resultFrom;
+let resultTo;
+let searchValue;
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
-  x += dx;
-  y += dy;
-}
-
-function startGame() {
-  const interval = setInterval(draw, 10);
-}
-
-document.getElementById("runButton").addEventListener("click", function () {
-  startGame();
-  this.disabled = true;
+// Event when currency is changed
+fromCurrecy.addEventListener('change', (event) => {
+	resultFrom = `${event.target.value}`;
 });
+
+// Event when currency is changed
+toCurrecy.addEventListener('change', (event) => {
+	resultTo = `${event.target.value}`;
+});
+
+search.addEventListener('input', updateValue);
+
+// Function for updating value
+function updateValue(e) {
+	searchValue = e.target.value;
+}
+
+// When user clicks, it calls function getresults 
+convert.addEventListener("click", getResults);
+
+// Function getresults
+function getResults() {
+	fetch(`${api}`)
+		.then(currency => {
+			return currency.json();
+		}).then(displayResults);
+}
+
+// Display results after conversion
+function displayResults(currency) {
+	let fromRate = currency.rates[resultFrom];
+	let toRate = currency.rates[resultTo];
+	finalValue.innerHTML =
+		((toRate / fromRate) * searchValue).toFixed(2);
+	finalAmount.style.display = "block";
+}
+
+// When user click on reset button
+function clearVal() {
+	window.location.reload();
+	document.getElementsByClassName("finalValue").innerHTML = "";
+};
+
+
 
 
