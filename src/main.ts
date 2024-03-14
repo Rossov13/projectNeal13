357a04e0f55c3059f78148f4c81e59191ff24ac3
Neal Rossov <nealrossov@gmail.com>
2024-03-14 20:45:23 +0300

    Breaking news)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
 
+
 // JS_PZ_Module_15_16_Week_21_1548323208.pdf
 
 // 1. HTML page with tabs.
@@ -639,5 +640,84 @@ document.addEventListener('keydown', (event) => {
   moveUserBlock(event)
 })
 
+
+
+
+
+// QUIZ GAME
+
+
+(function() {
+
+	let sprite = document.querySelector('.sprite'),
+		key = {left: false, right: false},
+		trans = 0,
+		property = getTransformProperty(sprite);
+
+
+
+	function getTransformProperty(element) {
+	    let properties = [
+	        'transform',
+	        'WebkitTransform',
+	        'msTransform',
+	        'MozTransform',
+	        'OTransform'
+	    ];
+	    let p;
+	    while (p = properties.shift()) {
+	        if (typeof element.style[p] != 'undefined') {
+	            return p;
+	        }
+	    }
+	    return false;
+	}
+
+	function translate() {
+		sprite.style[property] = 'translateX(' + trans + 'px)';
+	}
+$("#startWalk").click(walk)
+	function walk(e) {
+		let keyCode = e.keyCode;
+		if (keyCode === 39) {
+			key.right = true;
+		} else if (keyCode === 37) {
+			key.left = true;
+		}
+    if (key.right === true) {
+			trans += 10;
+			translate();
+			sprite.classList.remove('left');
+			sprite.classList.add('right');
+			sprite.classList.add('walk-right');
+		} else if (key.left === true) {
+			trans -= 10;
+			translate();
+			sprite.classList.remove('right');
+			sprite.classList.add('left');
+			sprite.classList.add('walk-left');
+		}
+	}
+
+
+
+	function stop(e) {
+		let keyCode = e.keyCode;
+		if (keyCode === 39) {
+			key.right = false;
+		} else if (keyCode === 37) {
+			key.left = false;
+		}
+		if (key.right === false) {
+			sprite.classList.remove('walk-right');
+		} if (key.left === false) {
+			sprite.classList.remove('walk-left');
+		}
+	}
+
+	document.addEventListener('keydown', walk, false);
+	document.addEventListener('keyup', stop, false);
+
+})();
 
 
