872b695b705cf72350534526332f76a54f1b62e7
neal rossov <nealrossov@gmail.com>
2024-03-15 18:06:21 +0300

    15.03.2024)

--- src/main.ts
+++ src/main.ts
@@ -647,77 +647,77 @@ document.addEventListener('keydown', (event) => {
 // QUIZ GAME
 
 
-(function() {
+// (function() {
 
-	let sprite = document.querySelector('.sprite'),
-		key = {left: false, right: false},
-		trans = 0,
-		property = getTransformProperty(sprite);
-
-
-
-	function getTransformProperty(element) {
-	    let properties = [
-	        'transform',
-	        'WebkitTransform',
-	        'msTransform',
-	        'MozTransform',
-	        'OTransform'
-	    ];
-	    let p;
-	    while (p = properties.shift()) {
-	        if (typeof element.style[p] != 'undefined') {
-	            return p;
-	        }
-	    }
-	    return false;
-	}
-
-	function translate() {
-		sprite.style[property] = 'translateX(' + trans + 'px)';
-	}
-$("#startWalk").click(walk)
-	function walk(e) {
-		let keyCode = e.keyCode;
-		if (keyCode === 39) {
-			key.right = true;
-		} else if (keyCode === 37) {
-			key.left = true;
-		}
-    if (key.right === true) {
-			trans += 10;
-			translate();
-			sprite.classList.remove('left');
-			sprite.classList.add('right');
-			sprite.classList.add('walk-right');
-		} else if (key.left === true) {
-			trans -= 10;
-			translate();
-			sprite.classList.remove('right');
-			sprite.classList.add('left');
-			sprite.classList.add('walk-left');
-		}
-	}
+// 	let sprite = document.querySelector('.sprite'),
+// 		key = {left: false, right: false},
+// 		trans = 0,
+// 		property = getTransformProperty(sprite);
 
 
 
-	function stop(e) {
-		let keyCode = e.keyCode;
-		if (keyCode === 39) {
-			key.right = false;
-		} else if (keyCode === 37) {
-			key.left = false;
-		}
-		if (key.right === false) {
-			sprite.classList.remove('walk-right');
-		} if (key.left === false) {
-			sprite.classList.remove('walk-left');
-		}
-	}
+// 	function getTransformProperty(element) {
+// 	    let properties = [
+// 	        'transform',
+// 	        'WebkitTransform',
+// 	        'msTransform',
+// 	        'MozTransform',
+// 	        'OTransform'
+// 	    ];
+// 	    let p;
+// 	    while (p = properties.shift()) {
+// 	        if (typeof element.style[p] != 'undefined') {
+// 	            return p;
+// 	        }
+// 	    }
+// 	    return false;
+// 	}
 
-	document.addEventListener('keydown', walk, false);
-	document.addEventListener('keyup', stop, false);
+// 	function translate() {
+// 		sprite.style[property] = 'translateX(' + trans + 'px)';
+// 	}
+// $("#startWalk").click(walk)
+// 	function walk(e) {
+// 		let keyCode = e.keyCode;
+// 		if (keyCode === 39) {
+// 			key.right = true;
+// 		} else if (keyCode === 37) {
+// 			key.left = true;
+// 		}
+//     if (key.right === true) {
+// 			trans += 10;
+// 			translate();
+// 			sprite.classList.remove('left');
+// 			sprite.classList.add('right');
+// 			sprite.classList.add('walk-right');
+// 		} else if (key.left === true) {
+// 			trans -= 10;
+// 			translate();
+// 			sprite.classList.remove('right');
+// 			sprite.classList.add('left');
+// 			sprite.classList.add('walk-left');
+// 		}
+// 	}
 
-})();
+
+
+// 	function stop(e) {
+// 		let keyCode = e.keyCode;
+// 		if (keyCode === 39) {
+// 			key.right = false;
+// 		} else if (keyCode === 37) {
+// 			key.left = false;
+// 		}
+// 		if (key.right === false) {
+// 			sprite.classList.remove('walk-right');
+// 		} if (key.left === false) {
+// 			sprite.classList.remove('walk-left');
+// 		}
+// 	}
+
+// 	document.addEventListener('keydown', walk, false);
+// 	document.addEventListener('keyup', stop, false);
+
+// })();
 
 
--- src/main1.ts
+++ src/main1.ts
@@ -1,3 +1,5 @@
+import './style.css'
+
 (function() {
 
 	let sprite = document.querySelector('.sprite'),
